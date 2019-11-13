e42deb0bcd9b319480d023ad5a1d18fde0ec7bdf
Yuvania Castillo <castilloyuvania15@gmail.com>
2019-11-13 11:34:25 -0600

    Agregar Atributos

---
+++ HogarAncianos/Model/Medicamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogarAncianos.Model
+{
+    class Medicamento
+    {
+        public string codigoMedicamento { get; set; }
+        public string nombreMedicamento { get; set; }
+        public string unidadMedida { get; set; }
+        public string categoria { get; set; }
+        public int catidadDisponible { get; set; }
+        public int cantidadPrescrita { get; set; }
+
+
+    }
+}
--- HogarAncianos/Model/Paciente.cs
+++ HogarAncianos/Model/Paciente.cs
@@ -8,5 +8,11 @@ namespace HogarAncianos.Model
 {
     class Paciente
     {
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string fechaNacimiento { get; set; }
+        public int edad { get; set; }
+        public string sexo { get; set; }
     }
 }
