6ed2d9bb0bb32bde2b6a87cef04069aa3f26c9b0
Robert Arias <robertariascastro@outlook.com>
2019-11-15 21:36:14 -0600

    Buscar empleados.

--- HogarAncianos/Controller/MenuController.cs
+++ HogarAncianos/Controller/MenuController.cs
@@ -6,12 +6,14 @@ namespace HogarAncianos.Controller {
 
         private FRM_MenuPrincipal frm_MenuPrincipal;
         private FRM_AgregarEmpleado frm_AgregarEmpleado;
+        private FRM_BuscarEmpleados frm_BuscarEmpleados;
         private FRM_AgregarMedicamento frm_AgregarMedicamento;
         private FRM_AgregarPaciente frm_AgregarPaciente;
 
         public MenuController(FRM_MenuPrincipal menuPrincipal) {
-            this.frm_MenuPrincipal = menuPrincipal;
+            frm_MenuPrincipal = menuPrincipal;
             frm_AgregarEmpleado = new FRM_AgregarEmpleado();
+            frm_BuscarEmpleados = new FRM_BuscarEmpleados();
             frm_AgregarPaciente = new FRM_AgregarPaciente();
             frm_AgregarMedicamento = new FRM_AgregarMedicamento();
             AgregarEventos();
@@ -19,7 +21,9 @@ namespace HogarAncianos.Controller {
 
         private void AgregarEventos() {
             frm_MenuPrincipal.mi_EmpleadosAgregar.Click += new EventHandler(OpenAgregarEmpleado);
+            frm_MenuPrincipal.mi_EmpleadosBuscar.Click += new EventHandler(OpenBuscarEmpleados);
             frm_AgregarEmpleado.FormClosed += CerrarAgregarEmpleado;
+            frm_BuscarEmpleados.FormClosed += CerrarBuscarEmpleados;
             frm_AgregarEmpleado.btnCancelar.Click += new EventHandler(CancelarAgregar);
 
             //Medicamentos 
@@ -33,18 +37,25 @@ namespace HogarAncianos.Controller {
             frm_AgregarPaciente.btnCancelar.Click += new EventHandler(CancelarAgregar);
         }
 
-      
-
-
         private void OpenAgregarEmpleado(object sender, EventArgs e) {
             frm_MenuPrincipal.Hide();
             frm_AgregarEmpleado.ShowDialog();
+        }
+
+        private void OpenBuscarEmpleados(object sender, EventArgs e) {
+            frm_MenuPrincipal.Hide();
+            frm_BuscarEmpleados.ShowDialog();
         }
 
         private void CerrarAgregarEmpleado(object sender, EventArgs e) {
             frm_AgregarEmpleado.Close();
             frm_MenuPrincipal.Show();
             frm_AgregarEmpleado.EstadoInicial();
+        }
+
+        private void CerrarBuscarEmpleados(object sender, EventArgs e) {
+            frm_BuscarEmpleados.Close();
+            frm_MenuPrincipal.Show();
         }
 
         private void CancelarAgregar(object sender, EventArgs e) {
