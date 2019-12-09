a43f6899eb0676a58405f6297b79ff28a806921f
Yuvania Castillo <castilloyuvania15@gmail.com>
2019-12-09 16:35:17 -0600

    INTERFAZ

--- HogarAncianos/View/FRM_ConsultaPrescripcion.cs
+++ HogarAncianos/View/FRM_ConsultaPrescripcion.cs
@@ -16,5 +16,10 @@ namespace HogarAncianos.View
         {
             InitializeComponent();
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
